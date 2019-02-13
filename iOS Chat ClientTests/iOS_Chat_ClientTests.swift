2cb76cfbb554de3c20d21bcdd1ea03f0cd8120f8
moldingtofu <jeremykchang@gmail.com>
2019-02-13 18:21:54 -0500

    initial commit

---
+++ iOS Chat ClientTests/iOS_Chat_ClientTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Chat_ClientTests.swift
+//  iOS Chat ClientTests
+//
+//  Created by jeremy on 2/13/19.
+//  Copyright © 2019 Jeremy Chang. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Chat_Client
+
+class iOS_Chat_ClientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
